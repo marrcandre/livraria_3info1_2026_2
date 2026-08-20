987925301c8a3038f6d71d3187108df950a95771
Marco André Mendes <marcoandre@gmail.com>
2026-08-20 11:13:23 -0300

    feat: inclusão do total da compra na listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -36,4 +36,4 @@ class CompraSerializer(ModelSerializer):
 
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'status', 'itens')
+        fields = ('id', 'usuario', 'status', 'total', 'itens')
