703919c79b29b72824da31a8ce92e9b29215079e
Marco André Mendes <marcoandre@gmail.com>
2026-09-18 10:54:32 -0300

    feat(35b): adicionando relatório de vendas mensal em compras

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,3 +1,8 @@
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Compra
@@ -28,3 +33,30 @@ class CompraViewSet(ModelViewSet):
             return Compra.objects.order_by('-id')
         return Compra.objects.filter(usuario=usuario).order_by('id')
 
+    @extend_schema(
+        request=None,
+        responses={200: None},
+        description="Gera um relatório de vendas do mês atual.",
+        summary="Relatório de vendas do mês",
+    )
+    @action(detail=False, methods=['get'])
+    def relatorio_vendas_mes(self, request):
+        agora = timezone.now()
+        inicio_mes = agora.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        compras = Compra.objects.filter(
+                status=Compra.StatusCompra.FINALIZADO,
+                data__gte=inicio_mes
+        )
+
+        total_vendas = sum(compra.total for compra in compras)
+        quantidade_vendas = compras.count()
+
+        return Response(
+            {
+                'status': 'Relatório de vendas deste mês',
+                'total_vendas': total_vendas,
+                'quantidade_vendas': quantidade_vendas,
+            },
+            status=status.HTTP_200_OK,
+        )
